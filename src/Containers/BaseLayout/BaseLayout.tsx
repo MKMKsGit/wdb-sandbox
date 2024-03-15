9ef891c2a6631a95c516c0815a863a439043e480
Ratchitta <ratchitta@skooldio.com>
2024-03-15 11:45:03 +0700

    Add BaseLayout component

---
+++ src/Containers/BaseLayout/BaseLayout.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react/jsx-runtime';
+import { Outlet } from 'react-router-dom';
+
+type BaseLayoutProps = {
+    children?: React.ReactNode;
+}
+
+const BaseLayout = (props: BaseLayoutProps) => {
+    const { children } = props;
+    return <Fragment><>Footer</>{children ?? <Outlet />}<>Navbar</></Fragment>;
+}
+
+export default BaseLayout;
