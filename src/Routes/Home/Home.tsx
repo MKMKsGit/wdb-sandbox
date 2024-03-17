257edd855cb10b8d0ae4af608a9a7a5ebe6ba104
Ratchitta <ratchitta.panyangam@gmail.com>
2024-03-17 22:39:42 +0700

    🚑 Add .env files and update paths for assets and public URLs

--- src/Routes/Home/Home.tsx
+++ src/Routes/Home/Home.tsx
@@ -4,10 +4,11 @@ import CollectionDetails from '@/Routes/Home/Containers/CollectionDetails';
 const Home = () => {
   return (
     <div>
-      <div
-        id="home-cover"
-        className={'h-[420px] bg-cover bg-center overflow-hidden bg-[url("assets/images/cover.png")]'}
-      ></div>
+      <img
+        className="w-full h-[420px] object-cover object-center overflow-hidden"
+        src={`${import.meta.env.VITE_PUBLIC_URL}assets/images/cover.png`}
+        alt="cover of wdb shop, 5 people walk on the street with colorful cloths"
+      />
       <div id="home-container" className={'px-40 pt-24 pb-40 flex flex-col justify-center'}>
         <CollectionDetails />
         <div id="home-product">
